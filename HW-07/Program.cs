3cf3bf9b06bf7e652b0cc69e3e5acb252560dae6
serge <szavistovich@gmail.com>
2022-08-16 19:18:47 +0300

    release v1

---
+++ HW-07/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе
+Console.Clear();
+
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+
+int result = n;
+int count = 0;
+
+while (result > 0)
+{
+    result = result / 10;
+    count++;
+}
+
+Console.WriteLine($"Number of digits in number {n} is {count}");
