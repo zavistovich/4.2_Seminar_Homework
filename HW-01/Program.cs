865bb192479f157cdd755fc57909d0ed4bd2e70d
serge <szavistovich@gmail.com>
2022-08-16 12:07:07 +0300

    release v1

---
+++ HW-01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+
+int number = new Random().Next(1, 8);
+string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+string result = "";
+
+if (number > 5) 
+{
+    result = $". It's a day off";
+}
+else 
+{
+    result = $", it's a working day";
+}
+
+Console.WriteLine($"{number} day of the week is {days[number - 1]}{result}");
