f5a21213495c9d99e204ff234ddc8a4daab14061
serge <szavistovich@gmail.com>
2022-08-16 19:28:10 +0300

    release v1

---
+++ HW-08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+
+int number = new Random().Next(1, 100);
+int sum = 0;
+Console.Write($"The sum of the digits of the number {number} is ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sum = sum + digit;
+    number = number / 10;
+}
+
+Console.WriteLine(sum);
