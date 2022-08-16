4e941f4c6919bad7701c9021cdbcf57423339310
serge <szavistovich@gmail.com>
2022-08-16 20:12:23 +0300

    release v1

---
+++ HW-09/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int Comp(int a)
+{
+    int comp = 1;
+    for (int num = 1; num <= a; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
+
+Console.Clear();
+
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine($"The product of numbers from 1 to {n} is {Comp(n)}");
