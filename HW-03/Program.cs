c326c1c0ec895d9325cf7233e368c6a01f9eb57c
serge <szavistovich@gmail.com>
2022-08-16 15:31:25 +0300

    release v1

---
+++ HW-03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+
+int quarter = new Random().Next(1,5);
+Console.Write($"For the quarter #{quarter} the coordinates must be: ");
+
+if (quarter == 1)
+{
+    Console.Write("x > 0, y > 0");
+}
+else if (quarter == 2)
+{
+    Console.Write("x < 0, y > 0");
+}
+else if (quarter == 3)
+{
+    Console.Write("x < 0, y < 0");
+}
+else if (quarter == 4)
+{
+    Console.Write("x > 0, y < 0");
+}
