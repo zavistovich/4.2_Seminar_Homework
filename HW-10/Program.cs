b7e6201c471e6d2ce51ad099c53b9d0db99c4c77
serge <szavistovich@gmail.com>
2022-08-16 20:28:24 +0300

    release v1

---
+++ HW-10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на чётную цифру
+Console.Clear();
+
+int a = new Random().Next(1, 10);
+int number = 1;
+
+
+while (number <= a)
+{
+    int cube = number * number * number;
+    
+    if (cube % 2 == 0) Console.Write(cube + " ");
+    number++;
+}
