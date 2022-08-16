fae524d939e8748370a4f35cbd79cd460d6a05c5
serge <szavistovich@gmail.com>
2022-08-16 19:08:48 +0300

    release v1

---
+++ HW-06/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму чисел от 1 до А
+int CountSum(int a)
+{
+    int number = 1;
+    int sum = 0;
+    for (int i = 0; i < a; i++)
+    {
+        sum = sum + number;
+        number++;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+Console.Write("Enter number: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+
+int sum = CountSum(a);
+Console.WriteLine($"The sum of numbers from 1 to {a} is {sum}");
